2ef0acf4eff053c44b8da4f955f6e42e870f86e7
Vincent Chov <vincent.chov@factset.com>
2021-04-08 14:40:20 -0400

    Revert "Test"
    This reverts commit 34ef7bc70b4e13da83572b107db1b1f166411368.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <h1>Original version</h1>
+      <ContactForm />
+
+      <h1>Xstate version</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactForm from "./components/ContactForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    ContactForm,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
